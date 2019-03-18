3c0079eb6215867a4cee127fe66bfe0add6596b5
Andrew Bruce <me@andrewbruce.net>
2019-03-18 14:04:23 +0000

    Initial Spring initializr
    Kotlin, 2.1.3, Java 11
    
    Signed-off-by: Benjamin Gröhbiel <ben@boclips.com>

---
+++ src/test/kotlin/com/boclips/terry/TerryApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.boclips.terry
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class TerryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
