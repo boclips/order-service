f5ac6c45a543a9afb0e305388436a34ef397a14b
Andrew Bruce <me@andrewbruce.net>
2019-03-18 15:04:56 +0000

    Add Web context and a basic homepage
    Signed-off-by: Benjamin Gröhbiel <ben@boclips.com>

---
+++ src/main/kotlin/com/boclips/terry/HomeController.kt
@@ -0,0 +1,10 @@
+package com.boclips.terry
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HomeController {
+	@GetMapping("/")
+	fun index() = "<h1>Do as I say, and do not do as I do</h1>"
+}
