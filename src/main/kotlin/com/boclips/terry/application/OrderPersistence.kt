8f49fb86ac17b651b9e117f8476b8db8d2167859
Andrew Bruce <me@andrewbruce.net>
2019-05-13 15:25:39 +0100

    Log errors when legacy order event handler fails
    Debug in prod FTW
    
    [#165441236]

--- src/main/kotlin/com/boclips/terry/application/OrderPersistence.kt
+++ src/main/kotlin/com/boclips/terry/application/OrderPersistence.kt
@@ -6,6 +6,7 @@ import com.boclips.terry.domain.Order
 import com.boclips.terry.domain.OrderStatus
 import com.boclips.terry.domain.OrdersRepository
 import com.boclips.terry.infrastructure.LegacyOrderDocument
+import mu.KLogging
 import org.springframework.cloud.stream.annotation.StreamListener
 import org.springframework.stereotype.Component
 
@@ -13,23 +14,29 @@ import org.springframework.stereotype.Component
 class OrderPersistence(
     private val repo: OrdersRepository
 ) {
+    companion object : KLogging()
+
     @StreamListener(Subscriptions.LEGACY_ORDER_SUBMITTED)
     fun onLegacyOrderSubmitted(event: LegacyOrderSubmitted) {
-        repo.add(
-            order = Order(
-                id = event.order.id,
-                uuid = event.order.uuid,
-                createdAt = event.order.dateCreated.toInstant(),
-                updatedAt = event.order.dateUpdated.toInstant(),
-                creator = event.order.creator,
-                vendor = event.order.vendor,
-                isbnOrProductNumber = event.order.extraFields.isbnOrProductNumber,
-                status = OrderStatus.parse(event.order.status)
-            ),
-            legacyDocument = LegacyOrderDocument(
-                order = event.order,
-                items = event.orderItems
+        try {
+            repo.add(
+                order = Order(
+                    id = event.order.id,
+                    uuid = event.order.uuid,
+                    createdAt = event.order.dateCreated.toInstant(),
+                    updatedAt = event.order.dateUpdated.toInstant(),
+                    creator = event.order.creator,
+                    vendor = event.order.vendor,
+                    isbnOrProductNumber = event.order.extraFields.isbnOrProductNumber,
+                    status = OrderStatus.parse(event.order.status)
+                ),
+                legacyDocument = LegacyOrderDocument(
+                    order = event.order,
+                    items = event.orderItems
+                )
             )
-        )
+        } catch (e: Exception) {
+            logger.error { "Couldn't process legacy order: $e" }
+        }
     }
 }
