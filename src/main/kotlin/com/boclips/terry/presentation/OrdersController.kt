ca8baeaa80a0e766df38b0e048a5395bd34c12fe
Alex Glen <alex@boclips.com>
2019-09-27 17:27:08 +0100

    Expose content-disposition headers for ExportCsv
    [#167810142]
    
    Signed-off-by: Antony O'Neill <antony@boclips.com>

--- src/main/kotlin/com/boclips/terry/presentation/OrdersController.kt
+++ src/main/kotlin/com/boclips/terry/presentation/OrdersController.kt
@@ -16,6 +16,7 @@ import org.springframework.hateoas.mvc.ControllerLinkBuilder
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -66,6 +67,7 @@ class OrdersController(
         .let(HateoasEmptyCollection::fixIfEmptyCollection)
         .let { Resources(it, getSelfOrdersLink()) }
 
+    @CrossOrigin(exposedHeaders = ["Content-Disposition"])
     @GetMapping(produces = ["text/csv"])
     fun getOrderCsv() : ResponseEntity<Any> =
         ResponseEntity(
