4f4b16b5c084c70fb735124a730c808a53ad8dc5
Matt Jones <matt@boclips.com>
2019-12-06 15:37:16 +0000

    Make CurrencyLayer fx service cacheable
    Signed-off-by: Jacek Rzeniewicz <jacek@boclips.com>

--- src/main/kotlin/com/boclips/orders/infrastructure/currency/CurrencyLayerFxRateService.kt
+++ src/main/kotlin/com/boclips/orders/infrastructure/currency/CurrencyLayerFxRateService.kt
@@ -1,26 +1,31 @@
 package com.boclips.orders.infrastructure.currency
 
 import com.boclips.orders.domain.service.currency.FxRateService
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.web.client.RestTemplate
 import java.math.BigDecimal
 import java.time.LocalDate
 import java.util.Currency
 
-class CurrencyLayerFxRateService(private val accessKey: String) : FxRateService {
+open class CurrencyLayerFxRateService(private val accessKey: String) : FxRateService {
 
     private val restTemplate = RestTemplate()
 
+    @Cacheable
     override fun getRate(from: Currency, to: Currency, on: LocalDate): BigDecimal {
-        val endpoint = "http://apilayer.net/api/historical?access_key={accessKey}&date={date}&source={from}&currencies={to}"
+        val endpoint =
+            "http://apilayer.net/api/historical?access_key={accessKey}&date={date}&source={from}&currencies={to}"
 
-        val response = restTemplate.getForObject(endpoint, CurrencyLayerResponse::class.java, mapOf(
-            "accessKey" to accessKey,
-            "from" to from.currencyCode,
-            "to" to to.currencyCode,
-            "date" to on.toString()
-        ))
+        val response = restTemplate.getForObject(
+            endpoint, CurrencyLayerResponse::class.java, mapOf(
+                "accessKey" to accessKey,
+                "from" to from.currencyCode,
+                "to" to to.currencyCode,
+                "date" to on.toString()
+            )
+        )
 
-        if(response?.success != true) {
+        if (response?.success != true) {
             throw Exception(response?.error?.info ?: "Failed to get fx rate")
         }
 
