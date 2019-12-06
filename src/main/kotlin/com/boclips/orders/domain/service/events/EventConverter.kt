af0adedb205896aa9f8004db14ebfcc5a51d64b5
Matt Jones <matt@boclips.com>
2019-12-06 17:17:12 +0000

    Add gbp price and customer org name to order events
    Signed-off-by: Jacek Rzeniewicz <jacek@boclips.com>

--- src/main/kotlin/com/boclips/orders/domain/service/events/EventConverter.kt
+++ src/main/kotlin/com/boclips/orders/domain/service/events/EventConverter.kt
@@ -1,7 +1,10 @@
 package com.boclips.orders.domain.service.events
 
 import com.boclips.eventbus.domain.video.VideoId
+import com.boclips.eventbus.events.order.OrderItem
 import com.boclips.orders.domain.model.Order
+import com.boclips.orders.domain.model.Price
+import java.math.BigDecimal
 import java.time.ZoneOffset
 import com.boclips.eventbus.events.order.Order as EventOrder
 
@@ -12,8 +15,24 @@ class EventConverter {
             .id(order.id.value)
             .createdAt(order.createdAt.atZone(ZoneOffset.UTC))
             .updatedAt(order.updatedAt.atZone(ZoneOffset.UTC))
+            .customerOrganisationName(order.organisation?.name ?: "UNKNOWN")
             .videoIds(order.items.map { VideoId(it.video.videoServiceId.value) })
+            .items(order.items.map { item ->
+                OrderItem.builder()
+                    .videoId(VideoId(item.video.videoServiceId.value))
+                    .priceGbp(getItemPriceInGbp(item.price, order))
+                    .build()
+            })
             .build()
     }
 
+    private fun getItemPriceInGbp(
+        price: Price,
+        order: Order
+    ): BigDecimal {
+        price.amount ?: return BigDecimal("0.00")
+        order.fxRateToGbp ?: return BigDecimal("0.00")
+
+        return price.amount.multiply(order.fxRateToGbp)
+    }
 }
--- src/test/kotlin/com/boclips/orders/domain/service/events/EventConverterTest.kt
+++ src/test/kotlin/com/boclips/orders/domain/service/events/EventConverterTest.kt
@@ -1,12 +1,18 @@
 package com.boclips.orders.domain.service.events
 
 import com.boclips.eventbus.domain.video.VideoId
+import com.boclips.eventbus.events.order.OrderItem
 import com.boclips.orders.domain.model.OrderId
+import com.boclips.orders.domain.model.OrderOrganisation
+import com.boclips.orders.domain.model.Price
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import testsupport.OrderFactory
+import testsupport.PriceFactory
 import testsupport.TestFactories
+import java.math.BigDecimal
 import java.time.ZonedDateTime
+import java.util.Currency
 
 class EventConverterTest {
 
@@ -16,8 +22,13 @@ class EventConverterTest {
             id = OrderId("the-id"),
             createdAt = ZonedDateTime.parse("2018-10-05T12:13:14Z").toInstant(),
             updatedAt = ZonedDateTime.parse("2019-10-05T12:13:14Z").toInstant(),
+            orderOrganisation = OrderOrganisation(name = "Pearson"),
+            fxRateToGbp = BigDecimal("2"),
             items = listOf(
-                OrderFactory.orderItem(video = TestFactories.video(videoServiceId = "the-video-id"))
+                OrderFactory.orderItem(
+                    video = TestFactories.video(videoServiceId = "the-video-id"),
+                    price = PriceFactory.tenDollars()
+                )
             )
         )
 
@@ -27,5 +38,44 @@ class EventConverterTest {
         assertThat(eventOrder.createdAt).isEqualTo("2018-10-05T12:13:14Z")
         assertThat(eventOrder.updatedAt).isEqualTo("2019-10-05T12:13:14Z")
         assertThat(eventOrder.videoIds).containsExactly(VideoId("the-video-id"))
+        assertThat(eventOrder.items).contains(
+            OrderItem.builder()
+                .priceGbp(BigDecimal("20.00"))
+                .videoId(VideoId("the-video-id"))
+                .build()
+        )
+        assertThat(eventOrder.customerOrganisationName).isEqualTo("Pearson")
+    }
+
+    @Test
+    fun `price is zero when order has no currency`() {
+        val order = OrderFactory.order(
+            currency = null, items = listOf(
+                OrderFactory.orderItem(
+                    video = TestFactories.video(),
+                    price = Price(amount = BigDecimal.TEN, currency = null)
+                )
+            )
+        )
+
+        val eventOrder = EventConverter().convertOrder(order)
+
+        assertThat(eventOrder.items.first().priceGbp).isEqualTo("0.00")
+    }
+
+    @Test
+    fun `price is zero when order has no price amount`() {
+        val order = OrderFactory.order(
+            currency = Currency.getInstance("USD"), items = listOf(
+                OrderFactory.orderItem(
+                    video = TestFactories.video(),
+                    price = Price(amount = null, currency = Currency.getInstance("USD"))
+                )
+            )
+        )
+
+        val eventOrder = EventConverter().convertOrder(order)
+
+        assertThat(eventOrder.items.first().priceGbp).isEqualTo("0.00")
     }
 }
