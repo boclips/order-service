39e1876e68a2f7b609fb135ab418ecb4e2f1fb22
Jacek Rzeniewicz <jacek@rzrz.pl>
2019-12-11 12:34:18 +0000

    Include order status in events

--- src/main/kotlin/com/boclips/orders/domain/service/events/EventConverter.kt
+++ src/main/kotlin/com/boclips/orders/domain/service/events/EventConverter.kt
@@ -3,16 +3,19 @@ package com.boclips.orders.domain.service.events
 import com.boclips.eventbus.domain.video.VideoId
 import com.boclips.eventbus.events.order.OrderItem
 import com.boclips.orders.domain.model.Order
+import com.boclips.orders.domain.model.OrderStatus
 import com.boclips.orders.domain.model.Price
 import java.math.BigDecimal
 import java.time.ZoneOffset
 import com.boclips.eventbus.events.order.Order as EventOrder
+import com.boclips.eventbus.events.order.OrderStatus as EventOrderStatus
 
 class EventConverter {
 
     fun convertOrder(order: Order): EventOrder {
         return EventOrder.builder()
             .id(order.id.value)
+            .status(convertOrderStatus(order.status))
             .createdAt(order.createdAt.atZone(ZoneOffset.UTC))
             .updatedAt(order.updatedAt.atZone(ZoneOffset.UTC))
             .customerOrganisationName(order.organisation?.name ?: "UNKNOWN")
@@ -23,6 +26,15 @@ class EventConverter {
                     .build()
             })
             .build()
+    }
+
+    fun convertOrderStatus(orderStatus: OrderStatus): EventOrderStatus {
+        return when(orderStatus) {
+            OrderStatus.COMPLETED -> EventOrderStatus.COMPLETED
+            OrderStatus.CANCELLED -> EventOrderStatus.CANCELLED
+            OrderStatus.INCOMPLETED -> EventOrderStatus.INCOMPLETED
+            OrderStatus.INVALID -> EventOrderStatus.INVALID
+        }
     }
 
     private fun getItemPriceInGbp(
--- src/test/kotlin/com/boclips/orders/domain/service/events/EventConverterTest.kt
+++ src/test/kotlin/com/boclips/orders/domain/service/events/EventConverterTest.kt
@@ -2,8 +2,10 @@ package com.boclips.orders.domain.service.events
 
 import com.boclips.eventbus.domain.video.VideoId
 import com.boclips.eventbus.events.order.OrderItem
+import com.boclips.orders.common.Do.exhaustive
 import com.boclips.orders.domain.model.OrderId
 import com.boclips.orders.domain.model.OrderOrganisation
+import com.boclips.orders.domain.model.OrderStatus
 import com.boclips.orders.domain.model.Price
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
@@ -13,13 +15,17 @@ import testsupport.TestFactories
 import java.math.BigDecimal
 import java.time.ZonedDateTime
 import java.util.Currency
+import com.boclips.eventbus.events.order.OrderStatus as EventOrderStatus
 
 class EventConverterTest {
+
+    val eventConverter = EventConverter()
 
     @Test
     fun `convert order`() {
         val order = OrderFactory.order(
             id = OrderId("the-id"),
+            status = OrderStatus.COMPLETED,
             createdAt = ZonedDateTime.parse("2018-10-05T12:13:14Z").toInstant(),
             updatedAt = ZonedDateTime.parse("2019-10-05T12:13:14Z").toInstant(),
             orderOrganisation = OrderOrganisation(name = "Pearson"),
@@ -32,9 +38,10 @@ class EventConverterTest {
             )
         )
 
-        val eventOrder = EventConverter().convertOrder(order)
+        val eventOrder = eventConverter.convertOrder(order)
 
         assertThat(eventOrder.id).isEqualTo("the-id")
+        assertThat(eventOrder.status).isEqualTo(EventOrderStatus.COMPLETED)
         assertThat(eventOrder.createdAt).isEqualTo("2018-10-05T12:13:14Z")
         assertThat(eventOrder.updatedAt).isEqualTo("2019-10-05T12:13:14Z")
         assertThat(eventOrder.items).contains(
@@ -44,6 +51,14 @@ class EventConverterTest {
                 .build()
         )
         assertThat(eventOrder.customerOrganisationName).isEqualTo("Pearson")
+    }
+
+    @Test
+    fun `convert order status`() {
+        assertThat(eventConverter.convertOrderStatus(OrderStatus.COMPLETED)).isEqualTo(EventOrderStatus.COMPLETED)
+        assertThat(eventConverter.convertOrderStatus(OrderStatus.CANCELLED)).isEqualTo(EventOrderStatus.CANCELLED)
+        assertThat(eventConverter.convertOrderStatus(OrderStatus.INCOMPLETED)).isEqualTo(EventOrderStatus.INCOMPLETED)
+        assertThat(eventConverter.convertOrderStatus(OrderStatus.INVALID)).isEqualTo(EventOrderStatus.INVALID)
     }
 
     @Test
@@ -57,7 +72,7 @@ class EventConverterTest {
             )
         )
 
-        val eventOrder = EventConverter().convertOrder(order)
+        val eventOrder = eventConverter.convertOrder(order)
 
         assertThat(eventOrder.items.first().priceGbp).isEqualTo("0.00")
     }
@@ -73,7 +88,7 @@ class EventConverterTest {
             )
         )
 
-        val eventOrder = EventConverter().convertOrder(order)
+        val eventOrder = eventConverter.convertOrder(order)
 
         assertThat(eventOrder.items.first().priceGbp).isEqualTo("0.00")
     }
