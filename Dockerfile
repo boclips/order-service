073a6197a32fc68b60528957ac5c2f21619f6793
Benjamin Gröhbiel <ben@boclips.com>
2020-01-17 14:58:37 +0000

    Rename terry to order-service
    Signed-off-by: Benjamin Gröhbiel <ben@boclips.com>

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM openjdk:11.0.2-jdk-stretch
-COPY build/libs/terry-*.jar /opt/app.jar
+COPY build/libs/order-service-*.jar /opt/app.jar
 WORKDIR /opt
 CMD ["java", "-jar", "app.jar"]
--- src/main/kotlin/com/boclips/orders/OrderServiceApplication.kt
+++ src/main/kotlin/com/boclips/orders/OrderServiceApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class TerryApplication
+class OrderServiceApplication
 
 fun main(args: Array<String>) {
-    runApplication<TerryApplication>(*args)
+    runApplication<OrderServiceApplication>(*args)
 }
