02fc477fb2ead181874c275ca3aeaef76d2498fd
Marcin Janik <marcin@boclips.com>
2020-12-16 12:05:12 +0100

    Remove unused userId from CartResource
    -it has already been removed from tests and api-client but
     was left out in the resource
    
     [#175935903]
    
    Signed-off-by: Remigiusz Oleszczuk <remy@boclips.com>

--- src/main/kotlin/com/boclips/orders/presentation/carts/CartResource.kt
+++ src/main/kotlin/com/boclips/orders/presentation/carts/CartResource.kt
@@ -4,12 +4,10 @@ import com.boclips.orders.domain.model.cart.Cart
 import org.springframework.hateoas.EntityModel
 
 class CartResource(
-    val userId: String,
     val items: List<EntityModel<CartItemResource>>
 ) {
     companion object {
         fun fromCart(cart: Cart) = CartResource(
-            userId = cart.userId.value,
             items = cart.items.map { CartItemResource.fromCartItem(it) }
         )
     }
